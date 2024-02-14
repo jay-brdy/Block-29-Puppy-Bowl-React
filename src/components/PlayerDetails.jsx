8be31bfd4cc2511c20f1550f6c6616ea343b6311
jay-brdy <jaysirit@gmail.com>
2024-02-14 01:43:10 -0600

    changes to image height

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -5,7 +5,7 @@ function PlayerDetails({ player }) {
         <div>
             <dialog open={player.id}>
                 <h2>{player.name}</h2>
-                <img src={player.imageUrl} alt={player.name} width={200} height={200} />
+                <img src={player.imageUrl} alt={player.name} width={200} height={275} />
                 <p>Breed: <strong>{player.breed}</strong></p>
             </dialog>
         </div>
